02d764c37fbdf89a013832f83b3f189cfae6fb88
Minus0149 <minus.official.01@gmail.com>
2023-10-19 21:15:47 +0530

    Done with the rough sketch of navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,9 @@
+import Nav from '@/components/nav'
 
 export default function Home() {
-  return <div className="bg-black text-white">hello</div>
+  return (
+    <div className="bg-black text-white">
+      <Nav />
+    </div>
+  )
 }
