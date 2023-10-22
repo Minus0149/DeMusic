20191fd33204ea57b3e5e4fd1ee287f2403be0e4
Minus0149 <minus.official.01@gmail.com>
2023-10-22 23:48:34 +0530

    Added a basic structure of three js and a scene

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,16 @@
-import Nav from '@/app/components/nav'
+'use client'
+
+import { Canvas } from '@react-three/fiber'
+import Nav from '@/app/components/Nav'
+import ThreeJsViewer from '@/app/components/ThreeJsViewer'
 
 export default function Home() {
   return (
     <div className="bg-black text-white">
       <Nav />
+      <Canvas>
+        <ThreeJsViewer />
+      </Canvas>
     </div>
   )
 }
