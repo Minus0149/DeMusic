d9d12baa9f84e1a5248ba712f7407915ca566855
Minus0149 <minus.official.01@gmail.com>
2023-10-23 13:31:30 +0530

    Added the model compressed it and did some minor changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
-import Nav from '@/app/components/Nav'
+'use client'
+
+import { Canvas } from '@react-three/fiber'
 import ThreeJsViewer from '@/app/components/ThreeJsViewer'
 
 export default function Home() {
   return (
-    <div className="bg-black text-white h-full overflow-x-hidden ">
-      <Nav />
-      <ThreeJsViewer />
+    <div className="bg-black text-white h-full w-full">
+      <Canvas camera={{ position: [0, 0, 4], fov: 45 }}>
+        <ThreeJsViewer />
+      </Canvas>
     </div>
   )
 }
