19c7fdc9b78e4d07bde9edfce7e0e077c6e9aa0b
Minus0149 <minus.official.01@gmail.com>
2023-10-24 22:15:15 +0530

    Did some css changes

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -11,7 +11,7 @@ const NavBar = () => {
   return (
     <Scroll html className="w-full">
       <div
-        className={`bg-zinc-950 absolute p-4 right-5 rounded-full top-5 items-center flex flex-col lg:hidded ${
+        className={`bg-zinc-950 absolute p-4 right-5 rounded-full top-5 items-center lg:hidden flex flex-col ${
           hamState && 'rounded-none top-0 right-0 w-full text-center'
         }`}
       >
