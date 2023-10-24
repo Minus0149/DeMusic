7ccc7b78da942aca50b690848a5cfbc02e33f1f2
Minus0149 <minus.official.01@gmail.com>
2023-10-24 19:07:38 +0530

    Added a bit of scroll animations

--- src/app/components/ThreeJsViewer.js
+++ src/app/components/ThreeJsViewer.js
@@ -1,39 +1,21 @@
 'use client'
-import {
-  Environment,
-  MeshReflectorMaterial,
-  OrbitControls,
-  ScrollControls,
-} from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
+import { Environment, OrbitControls, ScrollControls } from '@react-three/drei'
 import { Model } from './Model.js'
 import NavBar from '@/app/components/NavBar.js'
+import { Suspense } from 'react'
 
 const ThreeJsViewer = () => {
   return (
     <>
       <ambientLight />
       <OrbitControls enableZoom={false} makeDefault />
-      <ScrollControls pages={3} damping={0.25}>
+      <ScrollControls pages={1} damping={0.55}>
         <NavBar />
-        <Model />
+        <Suspense>
+          <Model />
+        </Suspense>
       </ScrollControls>
       <Environment preset="city" />
-      {/* <mesh rotation={[-Math.PI / 2, 0, 0]} position-y={-2}>
-        <planeGeometry args={[170, 170]} />
-        <MeshReflectorMaterial
-          blur={[300, 100]}
-          resolution={512}
-          mixBlur={0}
-          mixStrength={40}
-          roughness={1}
-          depthScale={1.2}
-          minDepthThreshold={0.4}
-          maxDepthThreshold={1}
-          color="#000"
-          metalness={0.3}
-        />
-      </mesh> */}
     </>
   )
 }
