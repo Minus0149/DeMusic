67a0dd98ea111b1e3ad4688a9c69129b56792850
Minus0149 <minus.official.01@gmail.com>
2023-10-24 22:00:08 +0530

    Added hamburger for mobile

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -1,13 +1,48 @@
 'use client'
-
+import { useState } from 'react'
 import { Scroll } from '@react-three/drei'
 import Link from 'next/link'
+import { FiX } from 'react-icons/fi'
+import { TiThMenuOutline } from 'react-icons/ti'
 
 const NavBar = () => {
+  const [hamState, setHamState] = useState(false)
+
   return (
-    <Scroll html className=" w-full">
-      <div className="bg-white h-16">
-        <div className="flex justify-around text-lg text-black pt-5 font-medium font-mono">
+    <Scroll html className="w-full">
+      <div
+        className={`bg-zinc-950 absolute p-4 right-5 rounded-full top-5 items-center flex flex-col lg:hidded ${
+          hamState && 'rounded-none top-0 right-0 w-full text-center'
+        }`}
+      >
+        <button onClick={() => setHamState(!hamState)}>
+          {hamState ? <FiX size={35} /> : <TiThMenuOutline size={35} />}
+        </button>
+
+        {hamState && (
+          <div className="lg:hidden flex flex-col items-center justify-around text-lg  pt-5 font-medium font-mono space-y-12">
+            <Link href="#">
+              <span className="font-extrabold">JARVIS</span>
+            </Link>
+            <ul className="flex flex-col space-y-12">
+              <li>
+                <Link href="#features">FEATURES</Link>
+              </li>
+              <li>
+                <Link href="#about">ABOUT</Link>
+              </li>
+              <li>
+                <Link href="#contact">CONTACT</Link>
+              </li>
+            </ul>
+            <Link href="/" className="font-medium">
+              <span className="font-extrabold">TRY JARVIS </span>It&apos;s free!
+            </Link>
+          </div>
+        )}
+      </div>
+      <div className="bg-white h-16 lg:block hidden">
+        <div className=" flex justify-around text-lg text-black pt-5 font-medium font-mono ">
           <Link href="#">
             <span className="font-extrabold">JARVIS</span>
           </Link>
